33069b8db85f007288e433d3eb14ae040ccc1a43
Peter Jiang <peter.j@usask.ca>
2025-01-23 13:25:09 -0600

    installed inquirer and qr-image pacakge from npm

--- Section 23_Node.js/2.4 QR Code Project/index.js
+++ Section 23_Node.js/2.4 QR Code Project/index.js
@@ -3,3 +3,11 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from 'inquirer';
+
+const prompt = inquirer.createPromptModule();
+
+inquirer.prompt(questions, answers) -> promise
+
+prompt(questions).then(/* ... */);
