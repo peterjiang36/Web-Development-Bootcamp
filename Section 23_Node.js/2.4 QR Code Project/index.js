7b24b8cbf5b1bc898db17fc80f0da27279596c6b
Peter Jiang <peter.j@usask.ca>
2025-01-23 14:30:24 -0600

    end of QR Code Generator Project

--- Section 23_Node.js/2.4 QR Code Project/index.js
+++ Section 23_Node.js/2.4 QR Code Project/index.js
@@ -5,7 +5,8 @@
 */
 
 import inquirer from "inquirer";
-// import from "qr-image";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([{
@@ -14,6 +15,14 @@ inquirer
 }])
   .then((answers) => {
     const url = answers.URL;
+
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
