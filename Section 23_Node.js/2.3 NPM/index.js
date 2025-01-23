2b51327145437bd79f1d5ea00a4e385be73e2ee8
Peter Jiang <peter.j@usask.ca>
2025-01-23 13:07:20 -0600

    superhero npm package

--- Section 23_Node.js/2.3 NPM/index.js
+++ Section 23_Node.js/2.3 NPM/index.js
@@ -1,4 +1,11 @@
-var generateName = require("sillyname");
-var sillyName = generateName();
+// import generateName from "sillyname";
+// var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+
+import {randomSuperhero} from 'superheroes';
+
+const name = randomSuperhero();
+
+console.log(`I am ${name}`);
