5b202d4dd8f04660266dfda9d89dc48b2badb1ea
Peter Jiang <peter.j@usask.ca>
2025-01-24 15:34:01 -0600

    hppt and postman usage

--- Section 24_Express.js with Node.js/3.2 HTTP Requests/index.js
+++ Section 24_Express.js with Node.js/3.2 HTTP Requests/index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello</h1>")
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>My name is Peter</p")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact</h1><p>Phone: +1 6472424568</p>")
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 })
