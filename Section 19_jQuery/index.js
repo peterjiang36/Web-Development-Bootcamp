ce10f640d5e7b7ff64450ff86afac757ac696499
Peter Jiang <peter.j@usask.ca>
2025-01-30 23:34:37 -0600

    jQuery for adding event listeners

--- Section 19_jQuery/index.js
+++ Section 19_jQuery/index.js
@@ -1,4 +1,9 @@
-$("h1").css({
-  "font-size": "100px",
-  color: "blue",
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function () {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+$("button").click(function () {
+  $("h1").css("color", "purple");
 });
